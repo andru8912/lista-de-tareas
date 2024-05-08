a1729954fc2ba6d35f3e989665562fcea739446a
Andrea <andru8912@hotmail.com>
2024-05-07 23:34:16 -0500

    se crea lista de tareas

---
+++ app.js
@@ -0,0 +1,40 @@
+const input = document.getElementById('ingresar-tarea');
+const boton = document.querySelector('button');
+const listaDeTarea = document.getElementById('lista-de-tareas');
+
+function agregarTarea() {
+    if (input.value) {
+        let tareaNueva = document.createElement('div');
+        tareaNueva.classList.add('tarea');
+
+        let texto = document.createElement('p');
+        texto.innerText = input.value;
+        tareaNueva.appendChild(texto);
+
+        let iconos = document.createElement('div');
+        iconos.classList.add('iconos');
+        tareaNueva.appendChild(iconos);
+
+        let completar = document.createElement('i');
+        completar.classList.add('bi', 'bi-check-circle-fill', 'icono-completar');
+        completar.addEventListener('click', completarTarea);
+
+        let eliminar = document.createElement('i');
+        eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-eliminar');
+
+        iconos.append(completar, eliminar);
+
+        listaDeTarea.appendChild(tareaNueva);
+      
+    }else{
+        alert('Por favor ingresa una tarea.');
+    }
+}
+
+function completarTarea(e) {
+    let tarea = e.target.parentNode.parentNode;
+    tarea.classList.toggle('completada');
+
+}
+
+boton.addEventListener('click', agregarTarea);
